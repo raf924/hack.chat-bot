2160f449069e57fec081d790f15bc2e35880fade
Rafaël Naciri <raf924@gmail.com>
2015-08-13 22:10:26 +0200

    Added fortune command

---
+++ src/commands/fortune.js
@@ -0,0 +1,8 @@
+module.exports.fortune = function(bot, sender, args, data) {
+  unirest.get("https://thibaultcha-fortunecow-v1.p.mashape.com/random")
+    .header("X-Mashape-Key", "54pyT5Ih3Vmsh7yUVh3vC3Nf2FuAp1MbTcjjsnj0of8Htcq5G2")
+    .header("Accept", "text/plain")
+    .end(function(result) {
+      bot.send(result.body);
+    });
+}
