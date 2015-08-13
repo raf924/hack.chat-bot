6daa5bbd27a7f77ca378923b64400c05c637a8f0
Rafaël Naciri <raf924@gmail.com>
2015-08-13 02:48:38 +0200

    Changed cmd character

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,17 +1,18 @@
-var help = function(bot, sender, args)
-{
-	var cmds = [];
-	var lvl = bot.permLevel[sender] || 0;
+var help = function (bot, sender, args) {
+    var cmds = [];
+    var lvl = bot.permLevel[sender] || 0;
 
-	for(var key in bot.commands)
-	{
-		var requiredLvl = bot.config.requiredPerm[key] || 0;
+    for (var key in bot.commands) {
+        var requiredLvl = bot.config.requiredPerm[key] || 0;
 
-		if(requiredLvl <= lvl)
-			cmds.push(key);
-	}
-	cmds = cmds.sort();
-	bot.send("$\\Large\\text{hack.chat bot by M4GNV5}$\nCommands: !" + cmds.join(", !"));
+        if (requiredLvl <= lvl)
+            cmds.push(key);
+    }
+    cmds = cmds.sort();
+    bot.send("$\\Large\\text{hack.chat bot by M4GNV5}$\nCommands: µ" + cmds.join(", µ"));
 }
 
-module.exports = { h: help, help: help };
+module.exports = {
+    h: help,
+    help: help
+};
