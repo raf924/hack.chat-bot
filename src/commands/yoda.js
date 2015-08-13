3827aab98e5107ab5834e73f7dceb38e86683876
Rafaël Naciri <raf924@gmail.com>
2015-08-13 02:36:22 +0200

    Fixed yoda command

--- src/commands/yoda.js
+++ src/commands/yoda.js
@@ -1,11 +1,12 @@
 var unirest = require("unirest");
 
 exports.yoda = function (bot, sender, args, data) {
-    unirest.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.")
+    var form_sentence = args.join(" ");
+    unirest.get("https://yoda.p.mashape.com/yoda?sentence=" + form_sentence)
         .header("X-Mashape-Key", "54pyT5Ih3Vmsh7yUVh3vC3Nf2FuAp1MbTcjjsnj0of8Htcq5G2")
         .header("Accept", "text/plain")
         .end(function (result) {
             var text = result.body;
             bot.send("@" + sender " Yoda would say : " + text);
         });
-}
+};
