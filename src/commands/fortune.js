8ebedd00d68da702dd1d4bb1d9c52689d4986a73
Rafaël Naciri <raf924@gmail.com>
2015-08-13 22:17:51 +0200

    Fixed fortune command

--- src/commands/fortune.js
+++ src/commands/fortune.js
@@ -1,3 +1,5 @@
+var unirest = require('unirest');
+
 module.exports.fortune = function(bot, sender, args, data) {
   unirest.get("https://thibaultcha-fortunecow-v1.p.mashape.com/random")
     .header("X-Mashape-Key", "54pyT5Ih3Vmsh7yUVh3vC3Nf2FuAp1MbTcjjsnj0of8Htcq5G2")
