e6f21d56784116c271e04f0b683b4ee002769058
Rafaël Naciri <raf924@gmail.com>
2015-08-16 20:36:49 +0200

    Added latest change display

--- src/commands/reddit.js
+++ src/commands/reddit.js
@@ -9,8 +9,8 @@ exports.reddit = function(bot, sender, args, data) {
   var out = [];
   redditWrap.r(r, function(err, data, res) {
     var posts = data.data.children;
-    for (var post of posts) {
-      out.push(post.data.title + " - http://redd.it/" + post.data.id);
+    for (var index in posts) {
+      out.push(post[index].data.title + " - http://redd.it/" + post[index].data.id);
       if(out.length===args[1]||3){
         return;
       }
