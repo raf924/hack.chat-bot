26bb9a95da1b312c5db07300724de5a7a1f8ca29
Rafaël Naciri <raf924@gmail.com>
2015-08-16 16:47:47 +0200

    Some changes

--- src/main.js
+++ src/main.js
@@ -4,67 +4,68 @@ var path = require("path");
 var ChatConnection = require("./connection.js");
 var config = require("./config.json");
 
-fs.readdir("./src/commands", function (err, files) {
-    if (err)
-        throw err;
+fs.readdir("./src/commands", function(err, files) {
+  if (err)
+    throw err;
 
-    var bot = new ChatConnection(config.url, config.nick, config.channel);
+  var channel = process.env.OPENSHIFT_NODE_PORT ? config.channel : config.testchannel;
+  var bot = new ChatConnection(config.url, config.nick, config.channel);
 
-    bot.commands = {};
-    bot.init = [];
+  bot.commands = {};
+  bot.init = [];
 
-    for (var i = 0; i < files.length; i++) {
-        if (path.extname(files[i]) == ".js") {
-            var cmds = require("./commands/" + files[i]);
+  for (var i = 0; i < files.length; i++) {
+    if (path.extname(files[i]) == ".js") {
+      var cmds = require("./commands/" + files[i]);
 
-            if (typeof cmds != 'object')
-                throw "Invalid command " + files[i];
+      if (typeof cmds != 'object')
+        throw "Invalid command " + files[i];
 
-            if (typeof cmds.init == 'function') {
-                cmds.init(bot);
-                bot.init.push(cmds.init);
-                delete cmds.init;
-            }
+      if (typeof cmds.init == 'function') {
+        cmds.init(bot);
+        bot.init.push(cmds.init);
+        delete cmds.init;
+      }
 
-            for (var key in cmds) {
-                if (typeof cmds[key] != 'function')
-                    throw "Invalid command " + files[i];
+      for (var key in cmds) {
+        if (typeof cmds[key] != 'function')
+          throw "Invalid command " + files[i];
 
-                bot.commands[key] = cmds[key];
-            }
-        }
+        bot.commands[key] = cmds[key];
+      }
     }
+  }
 
-    bot.parseCmd = function (data) {
-        console.log(data.nick + ": " + data.text);
+  bot.parseCmd = function(data) {
+    console.log(data.nick + ": " + data.text);
 
-        if (data.nick == config.nick)
-            return;
+    if (data.nick == config.nick)
+      return;
 
-        if (this.bans.indexOf(data.nick.toLowerCase()) !== -1)
-            return;
+    if (this.bans.indexOf(data.nick.toLowerCase()) !== -1)
+      return;
 
-        var msg = data.text;
-        if (msg[0] == "/") {
-            var cmd = msg.substr(1).split(" ")[0];
-            var args = msg.substr(2 + cmd.length).split(" ");
+    var msg = data.text;
+    if (msg[0] == "/") {
+      var cmd = msg.substr(1).split(" ")[0];
+      var args = msg.substr(2 + cmd.length).split(" ");
 
-            if (typeof this.commands[cmd] == 'function' && this.commands.hasOwnProperty(cmd))
-                this.commands[cmd](this, data.nick, args, data);
-            else
-                this.send("Unknown Command: " + cmd + ". Can't you people read the damn help??");
-        }
+      if (typeof this.commands[cmd] == 'function' && this.commands.hasOwnProperty(cmd))
+        this.commands[cmd](this, data.nick, args, data);
+      else
+        this.send("Unknown Command: " + cmd + ". Can't you people read the damn help??");
     }
+  }
 
-    bot.on("chat", function (data) {
-        bot.parseCmd(data);
-    });
+  bot.on("chat", function(data) {
+    bot.parseCmd(data);
+  });
 
-    bot.on("info", function (data) {
-        console.log("INFO : " + data.text);
-    });
+  bot.on("info", function(data) {
+    console.log("INFO : " + data.text);
+  });
 
-    bot.on("warn", function (data) {
-        console.log("WARN : " + data.text);
-    });
+  bot.on("warn", function(data) {
+    console.log("WARN : " + data.text);
+  });
 });
