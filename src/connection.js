2d0d8f1f11d4b2374862bff17e93306bddc969ab
Rafaël Naciri <raf924@gmail.com>
2015-08-16 20:43:31 +0200

    Added latest change display

--- src/connection.js
+++ src/connection.js
@@ -29,7 +29,7 @@ function ChatConnection(url, nick, channel) {
     that.ws.send(JSON.stringify(joinData));
     var cwd = process.cwd();
     try {
-      process.chdir(process.env.OPENSHIFT_HOME_DIR);
+      process.chdir(process.env.OPENSHIFT_HOME_DIR+"/git/nodejs.git");
     }
     catch(err){
 
