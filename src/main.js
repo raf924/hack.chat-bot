03cb9112660777dbc704b2c311ed8f5ef8d3193e
Rafaël Naciri <raf924@gmail.com>
2015-08-17 03:07:07 +0200

    Added latest change display

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Salut tout le monde !');
 });
+server.on("clientError", function (e) {
+  console.log(e);
+});
 server.listen(8080);
 
 setInterval(function() {
