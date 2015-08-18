cd821473c9d5373865e7b7bb9a2017bc63f33a39
Rafaël Naciri <raf924@gmail.com>
2015-08-18 16:51:17 +0200

    Added another source for jokes

--- src/commands/joke.js
+++ src/commands/joke.js
@@ -1,31 +1,42 @@
 var request = require("request");
 
 var jokes = [];
-request("http://reddit.com/r/jokes/.json", function(err, res, data)
-{
-	if(err)
-		throw err;
+request("http://reddit.com/r/jokes/.json", function(err, res, data) {
+  if (err)
+    throw err;
 
-	data = JSON.parse(data);
+  data = JSON.parse(data);
 
-	var posts = data.data.children;
+  var posts = data.data.children;
 
-	for(var i = 0; i < posts.length; i++)
-	{
-		var selfText = posts[i].data.selftext;
-		if(posts[i].data.domain != "self.Jokes" || selfText.split("\n").length > 10 || selfText.length > 500)
-			continue;
+  for (var i = 0; i < posts.length; i++) {
+    var selfText = posts[i].data.selftext;
+    if (posts[i].data.domain != "self.Jokes" || selfText.split("\n").length > 10 || selfText.length > 500)
+      continue;
 
-		var text = posts[i].data.title + "\n" + selfText + " - http://redd.it/" + posts[i].data.id;
-		jokes.push(text);
-	}
+    var text = posts[i].data.title + "\n" + selfText + " - http://redd.it/" + posts[i].data.id;
+    jokes.push(text);
+  }
 });
 
-var joke = function(bot, sender, args)
-{
-	var selected = Math.round(Math.random() * (jokes.length - 1));
+var joke = function(bot, sender, args) {
+  var selected = Math.round(Math.random() * (jokes.length - 1 * 2));
 
-	bot.send(jokes[selected]);
+  if (selected > jokes.length - 1) {
+    console.log("Extra jokes");
+    request("http://www.thejokeexchange.com/japi_send.php", function(err, res, data) {
+      var text = data;
+      text = text.replace(/<br>/gi, "");
+      text = text.replace(/<span .+>.+<[/]span>/gi, "");
+      text = text.replace(/\n\n/gi, "\n");
+      bot.send(text);
+    });
+  } else {
+    bot.send(jokes[selected]);
+  }
+
 };
 
-module.exports = { joke: joke };
+module.exports = {
+  joke: joke
+};
