94b83bbf8c484aa72922f1b3ae6d19797dea0f5b
Rafaël Naciri <raf924@gmail.com>
2015-09-04 22:40:02 +0200

    Prevent idle shutdown

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -1,5 +1,5 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT;
 
 var WebSocketServer = require('ws').Server
 var WebSocket = require("ws");
