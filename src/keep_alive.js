5a9296f3f39026685eb865dfeeae9e762c40521a
Rafaël Naciri <raf924@gmail.com>
2015-09-04 21:42:02 +0200

    Prevent idle shutdown

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -1,14 +1,38 @@
-var ws = require('ws');
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var WebSocketServer = ws.Server,
-  wss = new WebSocketServer({
-    port: 8000
+var WebSocketServer = require('ws').Server
+var WebSocket = require("ws");
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
   });
+  response.write("Welcome to Node.js on OpenShift!\n\n");
+  response.end("Thanks for visiting us! \n");
+});
 
-var socket = new ws("ws://" + process.env.OPENSHIFT_NODEJS_IP + ":8000");
+server.listen(port, ipaddress, function() {
+  console.log((new Date()) + ' Server is listening on port 8080');
+});
 
-socket.on("open", function() {
-  setInterval(function() {
+wss = new WebSocketServer({
+  server: server,
+  autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+
+var ws = WebSocket("http://nodejs-raf924.rhcloud.com:8000");
+ws.on("open", function () {
+  setInterval(function () {
     ws.send("ping");
   }, 1000 * 50);
 });
