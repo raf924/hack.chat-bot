a38075accc45e5c73e9d2cebac6aea0949bbcc09
Rafaël Naciri <raf924@gmail.com>
2015-09-04 21:31:20 +0200

    Prevent idle shutdown

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -2,10 +2,10 @@ var ws = require('ws');
 
 var WebSocketServer = ws.Server,
   wss = new WebSocketServer({
-    port: process.env.OPENSHIFT_NODEJS_PORT
+    port: 8000
   });
 
-var socket = new ws("http://" + process.env.OPENSHIFT_NODEJS_IP + ":" + process.env.OPENSHIFT_NODEJS_PORT);
+var socket = new ws("ws://" + process.env.OPENSHIFT_NODEJS_IP + ":8000");
 
 socket.on("open", function() {
   setInterval(function() {
